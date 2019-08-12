f765251d895f17a30260f8519bcb6dea90769e05
mithhu <mithhu666@gmail.com>
2019-08-12 20:03:15 +0600

    image issue fixed

--- js/app.js
+++ js/app.js
@@ -2,72 +2,50 @@ const quoteBox = document.getElementById("quote-box");
 let quoteCounter = 1;
 let quoteType = "All";
 let quoteTypeIndex;
-let quoteStart = 0;
-let quoteEnd = 21;
-let timeQuoteStart = 0;
-let timeQuoteEnd = 10;
-let moneyQuoteStart = 11;
-let moneyQuoteEnd = 21;
 
+//updates the counting of quote
 const updateQuoteCounter = () => {
   quoteCounter = document.getElementById("quote-counter").value;
   quoteBox.innerHTML = "";
 };
 
+//updates the quote type
 const updateQuoteType = () => {
   quoteTypeIndex = document.getElementById("quote-type");
   quoteType = quoteTypeIndex.options[quoteTypeIndex.selectedIndex].textContent;
   quoteBox.innerHTML = "";
 };
 
+//gets random index
 const getRandomIndex = (min, max) => {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
+
+//shows quote
 const showQuotes = () => {
   quoteBox.innerHTML = "";
-
   let arr = [];
-  if (quoteType === "Time") {
-    for (let i = 0; i < quoteCounter; i++) {
-      let para = document.createElement("p");
-      let randBegin = getRandomIndex(timeQuoteStart, timeQuoteEnd);
-      let randMiddle = getRandomIndex(timeQuoteStart, timeQuoteEnd);
-      let randEnd = getRandomIndex(timeQuoteStart, timeQuoteEnd);
-      let quote = `${quotes[randBegin].begin} ${quotes[randMiddle].middle} ${
-        quotes[randEnd].end
-      }`;
-      arr.push(quote);
-      para.innerHTML = quote;
-      quoteBox.append(para);
-    }
-  } else if (quoteType === "Money") {
-    for (let i = 0; i < quoteCounter; i++) {
-      let para = document.createElement("p");
-      let randBegin = getRandomIndex(moneyQuoteStart, moneyQuoteEnd);
-      let randMiddle = getRandomIndex(moneyQuoteStart, moneyQuoteEnd);
-      let randEnd = getRandomIndex(moneyQuoteStart, moneyQuoteEnd);
-      let quote = `${quotes[randBegin].begin} ${quotes[randMiddle].middle} ${
-        quotes[randEnd].end
-      }`;
-      arr.push(quote);
-      para.innerHTML = quote;
-      quoteBox.append(para);
-    }
-  } else {
-    for (let i = 0; i < quoteCounter; i++) {
-      let para = document.createElement("p");
-      let randBegin = getRandomIndex(quoteStart, quoteEnd);
-      let randMiddle = getRandomIndex(quoteStart, quoteEnd);
-      let randEnd = getRandomIndex(quoteStart, quoteEnd);
-      let quote = `${quotes[randBegin].begin} ${quotes[randMiddle].middle} ${
-        quotes[randEnd].end
-      }`;
-      arr.push(quote);
-      para.innerHTML = quote;
-      quoteBox.append(para);
-    }
+  let quoteSelection = quotes.filter(quote => quote.category === quoteType); //filters only the quotes of selected category
+
+  if (quoteType === "All") {
+    quoteSelection = quotes; //when all is selected, then quoteSelection variable stores the quotes array
   }
-  console.log(arr);
+
+  //loops according to quoteCounter
+  for (let i = 0; i < quoteCounter; i++) {
+    let para = document.createElement("p");
+    let randBegin = getRandomIndex(0, quoteSelection.length - 1); //selects the beginning part of sentence
+    let randMiddle = getRandomIndex(0, quoteSelection.length - 1); //selects the middle part of sentence
+    let randEnd = getRandomIndex(0, quoteSelection.length - 1); //selects the ending part of sentence
+    //concates all 3 part od sentence
+    let quote = `${quoteSelection[randBegin].begin} ${
+      quoteSelection[randMiddle].middle
+    } ${quoteSelection[randEnd].end}`;
+
+    arr.push(quote);
+    para.innerHTML = quote;
+    quoteBox.append(para);
+  }
 };
